6c64e3f042fa57448e60ca3bafd5ed7f20339cd3
KlaasHagge <44665897+KlaasHagge@users.noreply.github.com>
2023-07-03 14:33:06 +0200

    Arrays, Bedingungen

---
+++ Arrays_Bedingungen/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Reflection.Metadata;
+
+namespace Arrays_Bedingungen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] zahlen = { 2, 4, 45, -123, -8, 0, 11111 };
+
+            Console.WriteLine(zahlen[2]);
+            zahlen[2] = 1234;
+            Console.WriteLine(zahlen[2]);
+
+            Console.WriteLine(zahlen.Length);
+
+            Console.WriteLine(zahlen.Contains(-123));
+            Console.WriteLine(zahlen.Contains(555));
+
+
+            int a = 35;
+            int b = 35;
+
+
+            if(a < b)
+                Console.WriteLine("A ist kleiner als B");
+            else if (a == 34)
+            {
+                Console.WriteLine("A ist 34");
+            }
+            else if(a > b)
+            {
+                Console.WriteLine("A ist größer als B");
+            }
+            else
+            {
+                Console.WriteLine("A ist gleich B und nicht 34");
+            }
+
+
+            string erg = (a == b) ? "A ist gleich B" : "A ist ungleich B";
+
+            Console.WriteLine("ENDE DES IF-BLOCKS");
+        }
+    }
+}
--- HalloWelt/Program.cs
+++ HalloWelt/Program.cs
@@ -1,82 +1,122 @@
-﻿using System;
+﻿//Mittels der USING-Anweisungen kann ein vereinfachter Zugriff auf Programm-Externe Klassen ermöglicht werden. Es muss nun nicht mehr der
+///vollständige Pfad angegeben werden, sondern es reicht der Klassenbezeichner
+using System;
 
-namespace HalloWelt
+//NAMESPACE: Die Umgebung unseres aktuellen Programms: Alles innerhalb des Namespaces gehört zu dem Programm
+namespace HelloWorld
 {
-    internal class Program
+    //Die PROGRAM-Klasse beinhaltet den Einstiegspunkt des Programms und muss in jedem C#-Programm vorhanden sein
+    class Program
     {
+        //Die MAIN()-Methode ist der Einstiegspunkt jedes C#-Programms: Hier beginnt das Programm IMMER (auch wenn diese nicht explizit deklariert ist)
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hallo Welt!");
+            //Ausgabe eines String-Literals (Literale sind 'ausgeschriebene' Werte, im Gegensatz zu Werten die in einer Variablen stecken)
+            Console.WriteLine("Hello World!");
 
+            //Deklaration einer Integer-Variable 
             int Alter;
-
-            Alter = 34;
-
-            var Stadt = "Berlin";
-
+            //Initialisierung der Integer-Variablen
+            Alter = 32;
+            //Gleichzeitige Deklaration und Initialisierung einer String-Variablen
+            string Stadt = "Berlin";
+            //Ausgabe der Variablen
             Console.WriteLine(Alter);
             Console.WriteLine(Stadt);
-
+            //Deklaration und Initialisierung einer Integer-Variablen mithilfe einer anderen Integer-Variablen
             int DoppeltesAlter = Alter * 2;
             Console.WriteLine(DoppeltesAlter);
+            //Int-Berechnung innerhalb String-Ausgabe
+            Console.WriteLine(Alter + DoppeltesAlter);
 
+            //Beispiel-Char-Variable
             char Textzeichen = 'A';
+            //Beispiel-Double-Variable
+            double Kosten = 78.123;
+            Console.WriteLine(Kosten);
+            //Beispiel Bool-Variable
+            bool wahrheitswert = true;
 
-            bool Wahrheitswert = true;
 
-            double Gleitkomma = 45.12;
-            Console.WriteLine(Gleitkomma);
-
-
+            ///Einfügen dynamischer Inhalte in Strings
+            //'traditionell' über Stringverknüpfung (+-Operator)
             string Satz = "Ich bin " + Alter.ToString() + " Jahre alt und wohne in " + Stadt + ".";
             Console.WriteLine(Satz);
-            Console.WriteLine("Ich bin " + Alter.ToString() + " Jahre alt und wohne in " + Stadt + ".");
-
+            Console.WriteLine("Ich bin " + Alter + " Jahre alt und wohne in " + Stadt + ".");
+            //$-Operator (Variablen werde direkt in {}-Klammern geschrieben)
             Satz = $"Ich bin {Alter} Jahre alt und wohne in {Stadt}.";
             Console.WriteLine(Satz);
-            Console.WriteLine($"Ich bin {Alter} Jahre alt und wohne in {Stadt}.");
+            Console.WriteLine($"Ich bin {Alter.ToString()} Jahre alt und wohne in {Stadt}.");
+            //Index (Variablen werden durch Index-Platzhalter vertreten und später definiert)
+            Console.WriteLine("Ich bin {0} Jahre alt und wohne in {1}.", Alter, Stadt);
 
-            Console.WriteLine("Ich bin {0:0.0} Jahre alt und wohne in {1}.", Alter, Stadt);
+            //Ausgabe einer Berchnung in Strings
+            int a = 45;
+            int b = 12;
+            Console.WriteLine($"{a} + {b} = {a + b}");
 
-            int a = 12;
-            int b = 34;
-            Console.WriteLine($"{a} + {b} = {a+b}");
-
+            //String-Formatierung mittels Escape-Sequenzen
             string bsp = "Dies ist ein \tTabulator und dies ein \nZeilenumbruch.";
             Console.WriteLine(bsp);
+            //Bsp für Pfadausgabe mittels Escape-Sequenzen
+            string path = "C:\\Programme\\Programm.exe";
+            Console.WriteLine(path);
 
-            bsp = $"\"{{\\}}\"";
-            Console.WriteLine(bsp);
-
-            string verbatim = @"Dies ist ein    Tabulator und dies ein 
-Zeilenumbruch.";
+            //String-Formatierung mittels VerbaTim-String (Einleitung mittels @ / Escape-Sequenzen sind nicht möglich, dynamische Inhalte mittels $ schon)
+            string verbatim = @$"Dies ist ein    Tabulator und dies ein 
+Zeilenumbruch {Stadt}";
             Console.WriteLine(verbatim);
+            //Bsp für Pfadausgabe in Verbatim-String
+            path = @"C:\Programme\Programm.exe";
 
-            Console.Write("Gib deinen Namen ein: ");
+            //Formatierung mitels der String.Format-Funktion
+            string formatierteZahl = String.Format("{0:0.00}", Alter);
+            Console.WriteLine(formatierteZahl);
+
+
+            //Eingabe eines Strings durch den Benutzer und Abspeichern in einer String-Variablen
+            Console.WriteLine("Bitte gib deinen Namen an:");
             string eingabe = Console.ReadLine();
-            Console.WriteLine($"Du heißt also {eingabe}");
+            //Ausgabe
+            Console.WriteLine($"Dein Name ist also {eingabe}.");
 
+            //Benutzereingabe (Tastendruck) hier als Programmpause, bis Benutzer einer Taste drückt
             Console.ReadKey();
 
-            Console.Write("Gib deine Lieblingszahl ein: ");
-            int zahl = int.Parse(Console.ReadLine());
-            Console.WriteLine(zahl * 2);
+            //Eingabe eines Strings, Umwandlung in einen Integer (Parse()-Funktion) und Abspeichern in einer Integer-Variablen
+            Console.WriteLine("Bitte gib deine Lieblingszahl ein:");
+            string zahlAlsString = Console.ReadLine();
+            int zahl = int.Parse(zahlAlsString);
+            zahl = zahl * 2;
+            //Ausgabe
+            Console.WriteLine(zahl);
 
-            zahl = System.Convert.ToInt32(56.789);
 
+            //Umwandlung durch Convert.To[]()-Funktion
+            zahl = Convert.ToInt32(78.45);
 
-            int intZahl = 45;
+            //Bsp für numerische Umwandlung (implizit, da kein Datenverlust)
+            int intZahl = 78;
             double doubleZahl = intZahl;
-            Console.WriteLine(doubleZahl);
 
-            doubleZahl = 45.78;
+            //Bsp für numerische Umwandlung mittels Cast (explizit, da möglicherweise Datenverlust)
+            doubleZahl = 45.75;
             intZahl = (int)doubleZahl;
-            Console.WriteLine(intZahl);
 
+            //Umwandlung Int->Byte per Cast
+            //Achtung: Bei zu großer/kleiner Zahl wird kein Fehler ausgegeben
+            byte x = (byte)zahl;
+            Console.WriteLine(x);
+
+            //Umwandlung Int->Byte per Convert.ToByte-Funktion
+            //Zu große/kleine Zahl produziert Fehler
+            x = Convert.ToByte(zahl);
+            Console.WriteLine(x);
+
+            //Bsp für Teilung durch 0 von Gleitkommazahlen
             double zero = 0.0;
-            double erg = 67 / zero;
-            Console.WriteLine(erg);
-
+            double z = 2 / zero;
+            Console.WriteLine(z);
         }
     }
 }
