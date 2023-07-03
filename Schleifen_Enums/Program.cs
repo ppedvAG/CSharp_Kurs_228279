8e57d63ea34d7572e1cb5fa057640711c258e56a
KlaasHagge <44665897+KlaasHagge@users.noreply.github.com>
2023-07-03 16:53:24 +0200

    Tag 1

--- Schleifen_Enums/Program.cs
+++ Schleifen_Enums/Program.cs
@@ -4,112 +4,140 @@
     {
         static void Main(string[] args)
         {
+            #region Schleifen
+
             int a = 0;
             int b = 10;
 
+            //WHILE-Schleife
+            ///Die WHILE-Schleife wird wiederholt, solange die Bedingung wahr ist. Ist die Bedingung von vornherein unwahr, dann wird die Schleife
+            ///übersprungen
             while (a < b)
             {
-                Console.WriteLine("A ist kleiner als B");
-
+                Console.WriteLine("A ist kleiner B");
                 a++;
 
+                //Mit der BREAK-Anweisung wird die Schleife verlassen und der Code wird fortgesetzt.
                 if (a == 5)
                     break;
             }
-
-            Console.WriteLine("ENDE");
+            Console.WriteLine("Schleifenende");
 
             a = -45;
-
+            //DO-WHILE-Schleife
+            ///Auch die DO-WHILE-Schleife wird wiederholt, solange die Bedingung wahr ist. Allerdings wird die Bedingung erst am Schleifen_
+            ///ende geprüft, weshalb die Schleife mindestens einmal durchläuft.
             do
             {
                 Console.WriteLine(a);
                 a--;
 
+                //Der CONTINUE-Befehl beendet den aktuellen Schleifendurchlauf und lässt erneut die Bedingung prüfen. Ist die Bedingung wahr
+                ///beginnt ein neuer Durchlauf
                 continue;
-
-                a--;
 
             } while (a > 0);
 
 
-            for (int index = 0; index < 10; index+=2)
+            //FOR-Schleife
+            ///Der FOR-Schleife wird ein Laufindex (i) sowie eine Bedingung und eine Anweisung übergeben. Am Ende jedes Durchlaufes wird die
+            ///Anweisung ausgeführt. Wenn die Bedingung nicht (mehr) wahr ist, wird kein (weiterer) Schleifendurchlauf begonnen.
+            for (int i = 0; i < 10; i += 2)
             {
-                Console.WriteLine(index);
+                Console.WriteLine(i);
 
-                index++;
+                i++;
             }
 
-
-            int[] zahlen = { 2, 45, 1, -3, 56, 68 };
-
+            int[] zahlen = { 2, 3, 5, 4 };
+            //Iteration über ein Array mittels For-Schleife
             for (int i = 0; i < zahlen.Length; i++)
             {
                 Console.WriteLine(zahlen[i]);
             }
 
-            foreach (var item in zahlen)
+            //FOREACH-Schleifen können über Collections laufen und sprechen dabei jedes Element genau einmal an
+            foreach (int item in zahlen)
             {
                 Console.WriteLine(item);
             }
+            #endregion
 
+            #region Enums
 
-            Wochentag heutigerTag = Wochentag.Montag;
+            //ENUMs sind spezialisierte selbst-definierte Datentypen mit festgelegten möglichen Zuständen.
+            ///Dabei ist jeder Zustand an einen Integer-Wert gekoppelt, wodurch eine explizite Umwandlung (Cast) möglich ist. (vgl. Datentyp-Definition unten)
 
-            Console.WriteLine($"Heute ist {heutigerTag}.");
+            //Deklaration und Initialisierung einer Variablen vom Enumerator-Typ
+            Wochentag heutigerTag = Wochentag.Dienstag;
+            Console.WriteLine($"Heute ist also {heutigerTag}.");
 
-
-            Console.WriteLine("Welchen Wochentag magst du am liebsten?");
-            for (int i = 1; i <= Enum.GetValues(typeof(Wochentag)).Length; i++)
+            //For-Schleife über die möglichen Zustande des Enumerators
+            Console.WriteLine("Welcher Wochentag ist dein Lieblingstag?");
+            for (int i = 1; i < Enum.GetValues(typeof(Wochentag)).Length; i++)
             {
                 Console.WriteLine($"{i}: {(Wochentag)i}");
             }
+            //Speichern einer Benutzereingabe (Int) als Enumerator
+            //Cast: Int -> Wochentag
             heutigerTag = (Wochentag)int.Parse(Console.ReadLine());
-            Console.WriteLine($"Du magst {heutigerTag} besonders.");
+            Console.WriteLine($"Dein Lieblingstag ist also {heutigerTag}.");
 
-            heutigerTag = (Wochentag)Enum.Parse(typeof(Wochentag), Console.ReadLine());
-            Console.WriteLine($"Du magst {heutigerTag} besonders.");
+            //Parsing eines Strings in den Enumzustand
+            heutigerTag = (Wochentag)Enum.Parse(typeof(Wochentag), "Freitag");
+            Console.WriteLine($"Dein Lieblingstag ist also {heutigerTag}.");
 
-            if(Console.ReadKey().Key == ConsoleKey.Enter)
-                Console.WriteLine("Du hast ENTER gedrückt.");
-
-
+            //SWITCHs sind eine verkürzte Schreibweise für IF-ELSE-Blöcke. Mögliche Zustände der übergebenen Variablen werden 
+            //in den CASES definiert
             switch (heutigerTag)
             {
                 case Wochentag.Montag:
                     Console.WriteLine("Wochenstart");
+                    //Jeder speziell definierte CASE muss mit einer BREAK-Anweisung beendet werden
                     break;
+                //Wenn in einem CASE keine Anweisungen geschrieben stehen kann auf den BREAK-Befehl verzichtet werden. In diesem Fall
+                //springt das Programm in diesem CASE zum Nachfolgenden
                 case Wochentag.Dienstag:
                 case Wochentag.Mittwoch:
                 case Wochentag.Donnerstag:
-                    Console.WriteLine("Wochentag");
+                    Console.WriteLine("Normaler Wochentag");
                     break;
                 case Wochentag.Freitag:
                 case Wochentag.Samstag:
                 case Wochentag.Sonntag:
                     Console.WriteLine("Wochenende");
                     break;
+                //Wenn die übergebene Variable keinen der vordefinierten Zustände erreicht, wird der DEFAULT-Fall ausgeführt
                 default:
-                    Console.WriteLine("FEHLER");
+                    Console.WriteLine("Fehlerhafte Eingabe");
                     break;
             }
 
-            int zahl = 23;
-
+            //Mittels des WHEN-Stichworts kann auf Eigenschaften des betrachteten Objekts näher eingegangen werden
+            int zahl = -45;
             switch (zahl)
             {
                 case 5:
-                    Console.WriteLine("Variable ist 5");
+                    Console.WriteLine("a ist 5");
                     break;
-                case int z when z < 8:
-                    Console.WriteLine("Variable ist < 8");
+                //zahl wird in z eingelegt (zu überprüfende Variable wird für Bedingungsprüfung vorbereitet)
+                //und mittels when auf eine Eigenschaft geprüft
+                case int z when z < 0:
+                    Console.WriteLine("a < 0");
                     break;
                 default:
                     break;
             }
+
+            //Bsp für von C# verwendetem Enum (hier ConsoleKey = Abbildung der Tastaturtasten)
+            if (Console.ReadKey().Key == ConsoleKey.Enter)
+                Console.WriteLine("Du hast Enter gedrückt");
+            #endregion
 
         }
     }
 
-    enum Wochentag { Montag = 1, Dienstag, Mittwoch, Donnerstag, Freitag, Samstag, Sonntag}
+    //Enum-Definition (vgl. Verwendung oben)
+    enum Wochentag { Montag = 1, Dienstag, Mittwoch, Donnerstag, Freitag, Samstag, Sonntag }
+
 }
