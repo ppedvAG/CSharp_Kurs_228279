ed6f8f850402db63715fac6343b02d008c843213
KlaasHagge <44665897+KlaasHagge@users.noreply.github.com>
2023-07-03 16:10:54 +0200

    Schleifen_Enums

--- Arrays_Bedingungen/Program.cs
+++ Arrays_Bedingungen/Program.cs
@@ -6,41 +6,90 @@ namespace Arrays_Bedingungen
     {
         static void Main(string[] args)
         {
-            int[] zahlen = { 2, 4, 45, -123, -8, 0, 11111 };
+            #region Arrays
 
+            //ARRAYS
+            ///Arrays sind Collections, welche mehrere Variablen eines Datentyps speichern können. Die Größe des Arrays muss bei der
+            ///Initialisierung entweder durch eine Zahl oder durch eine bestimmte Anzahl von spezifischen Elementen definiert werden.
+            int[] zahlen = { 2, 4, 78, -123, -8, 0, 11111 };
+            //Der Zugiff auf einzelne Array-Positionen erfolgt durch einen Nullbasierten Index
             Console.WriteLine(zahlen[2]);
             zahlen[2] = 1234;
             Console.WriteLine(zahlen[2]);
 
-            Console.WriteLine(zahlen.Length);
+            //Array-Deklaration ohne direkte Initialisierung der Positionen (Größe muss angegeben werden)
+            string[] worte = new string[5];
 
+            //Neuzuweisung eines neuen Arrays zu einer existierenden Arrayvariablen 
+            zahlen = new int[] { 2, 4 };
+
+            //Verwendung der Contains-Funktion eines Arrays (überprüft auf das Vorhandensein eines Elements)
             Console.WriteLine(zahlen.Contains(-123));
             Console.WriteLine(zahlen.Contains(555));
 
+            //Ausgabe der Länge (Anzahl der Elemente) des Arrays
+            Console.WriteLine(zahlen.Length);
 
-            int a = 35;
-            int b = 35;
+            //String als Char-Array
+            string beispiel = "Hallo";
+            Console.WriteLine(beispiel[2]);
 
+            //Mehrdimensionales Array
+            int[,] zweiDimArray = new int[3, 5];
+            zweiDimArray[0, 0] = 1;
 
-            if(a < b)
-                Console.WriteLine("A ist kleiner als B");
-            else if (a == 34)
+            for (int i = 0; i < 3; i++)
             {
-                Console.WriteLine("A ist 34");
+                for (int j = 0; j < 5; j++)
+                {
+                    zweiDimArray[i, j] = i * j;
+                }
             }
-            else if(a > b)
+            Console.WriteLine(zweiDimArray[2, 3]);
+            #endregion
+
+            #region Bedingungen (If/Else)
+
+            //Deklaration und Initialisierung von Beispiel-Variablen
+            int a = 23;
+            int b = 23;
+
+            //BEDINGUNGEN (IF) prüfen logische Aussagen (z.B. Vergleiche oder Funktionen mit bool-Rückgabe) und führen den folgenden Block bei einem TRUE aus
+            if (a == 20)
+            {
+                Console.WriteLine("A ist 20");
+            }
+
+            //IF-ELSEIF-ELSE-Block
+            ///Das Programm wird den ersten Block ausführen, bei welchem er auf eine wahre Bedingung trifft und dann am Ende des Blocks mit
+            ///dem Code weiter machen
+            if (a < b)
+            {
+                Console.WriteLine("A ist kleiner als B");
+            }
+            //Es kann beliebig viele ELSE-IF-Blöcke geben
+            else if (a > b)
             {
                 Console.WriteLine("A ist größer als B");
             }
+            //Wenn keine der Bedingungen wahr ist, wird der (optionale) ELSE-Block ausgeführt
             else
+                Console.WriteLine("A ist gleich B");
+
+            //Kurznotation:
+            //(Bedingung) ? TrueAusgabe : FalseAusgabe
+            string ergebnis = (a == b) ? "A ist gleich B" : "A ist ungleich B";
+
+            //Bsp eines String-Vergleichs
+            string name1 = "Hans";
+            string name2 = "Hans";
+
+            if (name1.Equals(name2))
             {
-                Console.WriteLine("A ist gleich B und nicht 34");
+                Console.WriteLine("Gleich");
             }
+            #endregion
 
-
-            string erg = (a == b) ? "A ist gleich B" : "A ist ungleich B";
-
-            Console.WriteLine("ENDE DES IF-BLOCKS");
         }
     }
 }
---
+++ Schleifen_Enums/Program.cs
@@ -0,0 +1,115 @@
+﻿namespace Schleifen_Enums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int b = 10;
+
+            while (a < b)
+            {
+                Console.WriteLine("A ist kleiner als B");
+
+                a++;
+
+                if (a == 5)
+                    break;
+            }
+
+            Console.WriteLine("ENDE");
+
+            a = -45;
+
+            do
+            {
+                Console.WriteLine(a);
+                a--;
+
+                continue;
+
+                a--;
+
+            } while (a > 0);
+
+
+            for (int index = 0; index < 10; index+=2)
+            {
+                Console.WriteLine(index);
+
+                index++;
+            }
+
+
+            int[] zahlen = { 2, 45, 1, -3, 56, 68 };
+
+            for (int i = 0; i < zahlen.Length; i++)
+            {
+                Console.WriteLine(zahlen[i]);
+            }
+
+            foreach (var item in zahlen)
+            {
+                Console.WriteLine(item);
+            }
+
+
+            Wochentag heutigerTag = Wochentag.Montag;
+
+            Console.WriteLine($"Heute ist {heutigerTag}.");
+
+
+            Console.WriteLine("Welchen Wochentag magst du am liebsten?");
+            for (int i = 1; i <= Enum.GetValues(typeof(Wochentag)).Length; i++)
+            {
+                Console.WriteLine($"{i}: {(Wochentag)i}");
+            }
+            heutigerTag = (Wochentag)int.Parse(Console.ReadLine());
+            Console.WriteLine($"Du magst {heutigerTag} besonders.");
+
+            heutigerTag = (Wochentag)Enum.Parse(typeof(Wochentag), Console.ReadLine());
+            Console.WriteLine($"Du magst {heutigerTag} besonders.");
+
+            if(Console.ReadKey().Key == ConsoleKey.Enter)
+                Console.WriteLine("Du hast ENTER gedrückt.");
+
+
+            switch (heutigerTag)
+            {
+                case Wochentag.Montag:
+                    Console.WriteLine("Wochenstart");
+                    break;
+                case Wochentag.Dienstag:
+                case Wochentag.Mittwoch:
+                case Wochentag.Donnerstag:
+                    Console.WriteLine("Wochentag");
+                    break;
+                case Wochentag.Freitag:
+                case Wochentag.Samstag:
+                case Wochentag.Sonntag:
+                    Console.WriteLine("Wochenende");
+                    break;
+                default:
+                    Console.WriteLine("FEHLER");
+                    break;
+            }
+
+            int zahl = 23;
+
+            switch (zahl)
+            {
+                case 5:
+                    Console.WriteLine("Variable ist 5");
+                    break;
+                case int z when z < 8:
+                    Console.WriteLine("Variable ist < 8");
+                    break;
+                default:
+                    break;
+            }
+
+        }
+    }
+
+    enum Wochentag { Montag = 1, Dienstag, Mittwoch, Donnerstag, Freitag, Samstag, Sonntag}
+}
