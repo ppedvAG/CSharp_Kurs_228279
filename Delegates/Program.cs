ea82cd1724584e804c5fea9fcebfd78689599242
KlaasHagge <44665897+KlaasHagge@users.noreply.github.com>
2023-07-06 12:27:39 +0200

    WinForms-Lab

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -1,66 +1,76 @@
-﻿namespace Delegates
+﻿//DELEGATES sind Variablen, in welchen eine oder mehrere Referenzen auf Methoden gespeichert werden können
+namespace Delegates
 {
+    //Definition eines eigenen Delegate-Datentypen. Die Signatur (Rückgabetyp, Übergabeparametertypen) definiert die Art von Methoden, welche hineingelegt werden können.
     public delegate int MeinDelegate(int a, int b);
 
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            //Deklaration einer Delegate-Variablen
             MeinDelegate delegateVariable;
-
-            delegateVariable = new MeinDelegate(Addiere);
+            //Zuweisung der Variablen mit einer Methode (s.u.)
             delegateVariable = Addiere;
-
-            int erg = delegateVariable(13, 45);
+            //Aufruf der Methode über Delegate-Variable
+            int erg = delegateVariable(45, 12);
             Console.WriteLine(erg);
 
+            //Neuzuweisung des Delegates
             delegateVariable = Subtrahiere;
-            erg = delegateVariable(13, 45);
-            Console.WriteLine(erg);
+            Console.WriteLine(delegateVariable(45, 12));
 
+            //per += können weitere Methoden hinzugefügt werden
             delegateVariable += Addiere;
             delegateVariable += Addiere;
             delegateVariable += Addiere;
             delegateVariable += Addiere;
-            delegateVariable += Addiere;
+            //Alle Methoden werden nacheinander ausgefühert, aber nur der Rückgabewert der letzten Methode wird zurückgegeben
+            Console.WriteLine(delegateVariable(45, 12));
 
-            erg = delegateVariable(13, 45);
-            Console.WriteLine(erg);
-
-
+            //Ausgabe aller referenzierter Methoden
             foreach (var item in delegateVariable.GetInvocationList())
             {
                 Console.WriteLine(item.Method);
             }
 
+            //per -= können Methoden aus dem Delegate gelöst werden
             delegateVariable -= Addiere;
-
+            //Löschen aller Referenzen
             delegateVariable = null;
 
-
+            //FUNC, ACTION und PREDICATE sind vordefinierte, generische Delegate-Datentypen
             Func<int, int, int> meinFunc = Addiere;
 
-
+            //Übergabe einer Methode an eine andere Methode
             FühreAus(meinFunc);
             FühreAus(Addiere);
 
-            List<string> Städteliste = new List<string>() { "Berlin", "Hamburg", "München", "Köln", "Düsseldorf" };
+            //Beispiel-Liste
+            List<string> Städteliste = new List<string>() { "München", "Berlin", "Hamburg", "Köln" };
+            string gefundeneStadt;
 
-            string ergebnis = Städteliste.Find(FindeStadtMitH);
-            Console.WriteLine(ergebnis);
-
-            ergebnis = Städteliste.Find
+            //Aufruf der Find()-Funktion mit Übergabe einer Methode zur Definition der inneren Logik
+            gefundeneStadt = Städteliste.Find(SucheStadtMitB);
+            //Übergabe der Methode als anonyme Methode
+            gefundeneStadt = Städteliste.Find
                 (
-                    delegate (string input)
+                    delegate (string stadt)
                     {
-                        return input.StartsWith('H');
+                        return stadt.StartsWith('B');
                     }
                 );
-            Console.WriteLine(ergebnis);
+            //Übergabe der anonymen Methode in Lambda-Schreibweise (lang und kurz)
+            gefundeneStadt = Städteliste.Find((string stadt) => { return stadt.StartsWith('B'); });
 
-            ergebnis = Städteliste.Find((string input) => { return input.StartsWith('H'); });
-            ergebnis = Städteliste.Find(input => input.StartsWith('H'));
+            gefundeneStadt = Städteliste.Find(stadt => stadt.StartsWith('B'));
 
+            //weiteres Lambda-Beispiel mit der OrderBy-Methode
+            Städteliste = Städteliste.OrderBy(stadt => stadt[0]).ToList();
+            foreach (var item in Städteliste)
+            {
+                Console.WriteLine(item);
+            }
 
             Städteliste = Städteliste.OrderBy(stadt => stadt.Length).ToList();
             foreach (var item in Städteliste)
@@ -68,31 +78,26 @@
                 Console.WriteLine(item);
             }
 
-            Städteliste = Städteliste.OrderBy(stadt => stadt[0]).ToList();
-            foreach (var item in Städteliste)
-            {
-                Console.WriteLine(item);
-            }
         }
 
-        public static bool FindeStadtMitH(string input)
+        //Beipiel-Methode für Übergabe an Find()
+        public static bool SucheStadtMitB(string stadt)
         {
-            return input.StartsWith('H');
+            return stadt.StartsWith('B');
         }
 
+        //Beispiel für Methode, welche einen Delegate als Übergabe erwartet
         public static void FühreAus(Func<int, int, int> mehrLogik)
         {
-            Console.WriteLine(mehrLogik(100, 200));
+            mehrLogik(45, 78);
         }
 
-        public static void FühreAus02(Func<int, int, int> mehrLogik) => Console.WriteLine(mehrLogik(100, 200));
-
+        //Beispielmethoden für obige Delegate-Variablen
         public static int Addiere(int a, int b)
         {
             Console.WriteLine("Addiere");
             return a + b;
         }
-
         public static int Subtrahiere(int a, int b)
         {
             Console.WriteLine("Subtrahiere");
--- WindowsFormsBsp/Form1.cs
+++ WindowsFormsBsp/Form1.cs
@@ -1,49 +1,72 @@
 namespace WindowsFormsBsp
 {
+    //Bsp-Enum
     enum TestEnum { Test1, Test2, Test3 }
 
+    //PARTIAL besagt, dass diese Klasse in mehrere Teile (Dateien) aufgeteilt ist
     public partial class MainWindow : Form
     {
+        //Konstruktor für das Form (Fenster)
         public MainWindow()
         {
+            //Diese Methode initialisiert alle im Designer definierten Objekte. Sie sollte die erste Methode im Konstruktor sein
             InitializeComponent();
 
+            //Neuzuweisung einer Property des Buttons
             Btn_KlickMich.BackColor = Color.Aqua;
 
+            //Hinzufügen eines weiteren EventHandlers zum Button.Click-Event
             Btn_KlickMich.Click += NochEinEventHandler;
 
             for (int i = 0; i < Enum.GetValues(typeof(TestEnum)).Length; i++)
             {
+                //Befüllung der ComboBox mit Enum-Elementen
                 Cbb_Enum.Items.Add((TestEnum)i);
             }
 
+            //Neuzuweisung einer Property der TextBox
             Tbx_Input.Text = $"Hallo{Environment.NewLine}Moin";
         }
 
+        //EventHandler-Methoden
+
+        //EventHandler, welche auf einen Klick auf den Button 'KlickMich" reagiert
         private void Btn_KlickMich_Click(object sender, EventArgs e)
         {
+            //Neuzuweisung einer Eigenschaft des Buttons
             Btn_KlickMich.Text = "Button wurde geklickt";
+
+            //Neuzuweisung einer Eigenschaft des Forms
             this.BackColor = Color.Yellow;
 
+            //Prüfung, ob in der ComboBox ein Element angewählt wurde
             if (Cbx_Haken.Checked)
+                //Neuzuweisung einer Eigenschaft des Buttons über den sender-Parameter
                 (sender as Button).BackColor = Color.LightCoral;
 
+            //Prüfung, ob in der ListBox ein Element angewählt wurde
             if (Lbx_Auswahl.SelectedItem != null)
+                //Übertrag des Elements in den Button.Text
                 Btn_KlickMich.Text = Lbx_Auswahl.SelectedItem.ToString();
 
+            //Prüfung, ob in der ComboBox ein Element angewählt wurde
             if (Cbb_Enum.SelectedItem != null)
             {
+                //Übertrag des Elements in das Label
                 TestEnum enumVar = (TestEnum)Cbb_Enum.SelectedItem;
                 Lbl_Output.Text = enumVar.ToString();
             }
 
+            //Übertrag des TextBoxTexts in den Titel des Forms
             this.Text = Tbx_Input.Text;
         }
 
+        //Weiterer Event-Handler, der im Konstruktor dem Button.Click-Event zugrordnet wurde (färbt Button-Text ein)
         private void NochEinEventHandler(object sender, EventArgs e) => (sender as Button).ForeColor = Color.White;
 
         private void schließenToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            //Aufruf einer MessageBox und Prüfung des geklickten Buttons
             if (MessageBox.Show(
                                 "Möchtest du das Fenster wirklich schließen?",
                                 "Beenden",
@@ -51,33 +74,38 @@ namespace WindowsFormsBsp
                                 MessageBoxIcon.Warning
                             ) == DialogResult.Yes)
 
+                //Schließend des Fensters
                 this.Close();
 
+            //Schließend der Applikation
             //Application.Exit();
         }
 
         private void neuesFensterToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            //Instanziieren eines neuen Forms
             Form neuesFenster = new MainWindow();
-
+            //Neuzuweisung einer Eigenschaft des neuen Fensters
             neuesFenster.Text = "Neues Fenster";
-
+            //Öffnen des Forms als gleichberechtigtes Fenster
             neuesFenster.Show();
         }
 
         private void neuesDialogFensterToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            //Instanziieren eines neuen Forms
             Form neuesFenster = new MainWindow();
-
+            //Neuzuweisung einer Eigenschaft des neuen Fensters
             neuesFenster.Text = "Neues Dialog-Fenster";
             (neuesFenster as MainWindow).Btn_Ok.Enabled = true;
-
+            //Öffen des Forms als Dialogfenster (muss exklusiv bearbeitet werden) und Überprüfung des zurückgegebenen DialogResults (s.u.)
             if (neuesFenster.ShowDialog() == DialogResult.OK)
                 Lbl_Output.Text = "Es wurde auf OK geklickt";
         }
 
         private void Btn_Ok_Click(object sender, EventArgs e)
         {
+            //Setzen des DialogResults des DialogFensters (wird von ShowDialog zurückgegeben)
             this.DialogResult = DialogResult.OK;
             this.Close();
         }
